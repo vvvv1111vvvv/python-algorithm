cd7a6ce946047cfba2cddb19909125e4d860d6f7
Replit user <>
2023-11-30 14:58:48 +0000

    feat: 큰수의법칙 수정

---
+++ code-test-practices/greedy/큰수의법칙.py
@@ -0,0 +1,19 @@
+#N, M, K를 입력받기
+n, m, k = map(int, input().split(""))
+#데이터를 공백으로 구분하여 입력받기
+data = list(map(int, input().split("")))
+#데이터 정렬
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+result = 0
+var = 0
+for i in range(0, m):
+  if i == 0:
+    var = first
+  elif i % k == 0:
+    var = second
+  else:
+    var = first
+  result += var
+print(result)
