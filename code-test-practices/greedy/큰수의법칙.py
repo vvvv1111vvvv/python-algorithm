b9f5771e5d1d117829d8a7ad63627f276bd9e410
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2023-12-01 00:55:58 +0900

    feat: add 큰수의법칙2.py

--- code-test-practices/greedy/큰수의법칙.py
+++ code-test-practices/greedy/큰수의법칙.py
@@ -1,7 +1,7 @@
 #N, M, K를 입력받기
-n, m, k = map(int, input().split(""))
+n, m, k = map(int, input().split(" "))
 #데이터를 공백으로 구분하여 입력받기
-data = list(map(int, input().split("")))
+data = list(map(int, input().split(" ")))
 #데이터 정렬
 data.sort()
 first = data[n - 1]
